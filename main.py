fa859e73e508daec69178ff062a442ae4f3e0ee8
Gabriel <contato.gabrielazevedo0@gmail.com>
2025-05-19 17:06:56 -0300

    identificando a equipe

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ def main():
         return
 
     lexer.input(data)
-
+    print (
+        "Código da Equipe: EQ03\n"
+        "Componentes:\n"
+            "   Gabriel de Abreu Farias Azevedo;  gabriel.azevedo@ucsal.edu.br;	\n"
+            "   Rebeca Bezerra Gonçalves dos Santos;  rebecabezerra.santos@ucsal.edu.br; \n"
+            "   Sarah Evellyn Ferreira Nogueira;  sarahevellyn.nogueira@ucsal.edu.br; \n"
+            "   Robert Valadares de Matos;  robert.matos@ucsal.edu.br; \n"
+    )
     print("Tokens reconhecidos:\n")
     while True:
         tok = lexer.token()
         if not tok:
             break
-        print(f"Linha {tok.lineno} | Tipo: {tok.type} | Valor: {tok.value}")
+        print(f"| Linha {tok.lineno} | Tipo: {tok.type} | Valor: {tok.value}")
 
 if __name__ == "__main__":
     main()
